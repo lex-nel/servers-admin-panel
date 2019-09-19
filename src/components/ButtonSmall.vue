06d8e28a1ce562034cd0c094f4839bc13c999943
banana_bernard <telio58ob@gmail.com>
2019-09-19 16:05:29 +0700

    nice

---
+++ src/components/ButtonSmall.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+button
+  | STOP
+</template>
+
+<style lang="stylus">
+button
+  width: 260px
+  height: 66px
+  background: #43A09B
+  box-shadow: 4px 4px 15px rgba(0, 0, 0, 0.25)
+</style>
